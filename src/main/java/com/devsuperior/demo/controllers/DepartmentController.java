7c68b306704811a5a114e665a689918899895c2a
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2024-03-26 22:16:54 -0300

    .

---
+++ src/main/java/com/devsuperior/demo/controllers/DepartmentController.java
@@ -0,0 +1,28 @@
+package com.devsuperior.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.demo.dto.DepartmentDTO;
+import com.devsuperior.demo.services.DepartmentService;
+
+@RestController
+@RequestMapping(value = "/departments")
+public class DepartmentController {
+
+	@Autowired
+	private DepartmentService service;
+	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
+	@GetMapping
+	public ResponseEntity<List<DepartmentDTO>> findAll() {
+		List<DepartmentDTO> list = service.findAll();		
+		return ResponseEntity.ok().body(list);
+	}
+}
---
+++ src/main/java/com/devsuperior/demo/dto/EmployeeDTO.java
@@ -0,0 +1,70 @@
+package com.devsuperior.demo.dto;
+
+import com.devsuperior.demo.entities.Employee;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class EmployeeDTO {
+	
+	private Long id;
+	
+	@NotBlank(message = "Campo requerido")
+	private String name;
+
+	@Email(message = "Email inválido")
+	private String email;
+	
+	@NotNull(message = "Campo requerido")
+	private Long departmentId;
+	
+	public EmployeeDTO() {
+	}
+
+	public EmployeeDTO(Long id, String name, String email, Long departmentId) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.departmentId = departmentId;
+	}
+
+	public EmployeeDTO(Employee entity) {
+		id = entity.getId();
+		name = entity.getName();
+		email = entity.getEmail();
+		departmentId = entity.getDepartment().getId();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+}
